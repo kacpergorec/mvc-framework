d5f90d029f81bd59744745ae4e83cbae7c269a19
kacper <kacpergorec@gmail.com>
2022-08-23 23:54:56 +0200

    Database connection + DOTENV integration

--- core/Application.php
+++ core/Application.php
@@ -8,16 +8,19 @@ class Application
     public Request $request;
     public Response $response;
     public Controller $controller;
+    public Database $db;
     public static Application $app;
     public static string $ROOT_DIR;
 
-    public function __construct()
+    public function __construct($rootDir, array $config)
     {
-        self::$ROOT_DIR = dirname(__DIR__);
+        self::$ROOT_DIR = $rootDir;
         self::$app = $this;
         $this->request = new Request();
         $this->response = new Response();
         $this->router = new Router($this->request, $this->response);
+
+        $this->db = new Database($config['db']);
     }
 
     public function run()
--- public/index.php
+++ public/index.php
@@ -3,12 +3,21 @@
 use App\Controller\AuthController;
 use App\Controller\SiteController;
 use App\Core\Application;
+use Dotenv\Dotenv;
 
 require_once __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
 
+$config = [
+    'db' => [
+        'dsn' => $_ENV['DB_DSN'],
+        'user' => $_ENV['DB_USER'],
+        'password' => $_ENV['DB_PASSWORD']
+    ]
+];
 
-$app = new Application();
-
+$app = new Application(dirname(__DIR__), $config);
 
 
 //
